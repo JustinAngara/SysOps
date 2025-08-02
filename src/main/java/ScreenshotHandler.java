2e139e6bec388bd5169761d6622df516f2d4946f
Justin Angara <justin.angara@gmail.com>
2025-08-02 15:16:45 -0500

    added gui

---
+++ src/main/java/ScreenshotHandler.java
@@ -0,0 +1,8 @@
+public class ScreenshotHandler {
+    private final int X = 0;
+    private final int Y = 0;
+    private final int WIDTH = 1920;
+    private final int HEIGHT = 1080;
+
+    // time to get images ready for parsing
+}
